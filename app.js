85b32c0f42eaf25a99c56f7ecd23f9424ee9b55e
Arslan Jameel <arslanjameel40@gmail.com>
2021-11-10 15:02:16 +0500

    signup login and password encrpted

---
+++ app.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt=require('mongoose-encryption');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+// Connection url
+const uri = 'mongodb://localhost:27017/';
+// Database name
+const dbName = 'userDb';
+// Create a new connection
+mongoose.connect(uri + dbName, { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+const secret=process.env.SECRET;
+userSchema.plugin(encrypt,{secret:secret,encryptedFields: ['paassword']});
+
+const User = mongoose.model("User", userSchema);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', { errMsg: "", username: "" });
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(err => {
+        if (!err) {
+            res.render('Secrets');
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    User.findOne({ email: email }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                } else {
+                    res.render('login', { errMsg: "Email or password incorrect", username: email });
+                }
+            } else {
+                res.render('login', { errMsg: "Email or password incorrect", username: email });
+            }
+        }
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log("server is listening on http://localhost:" + port);
+});

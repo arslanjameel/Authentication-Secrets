802448e21645c7f4e2c1d9ffcffc90650d6b3aca
Arslan Jameel <arslanjameel40@gmail.com>
2021-11-10 16:10:19 +0500

    remove encrypt and hash password

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt=require('mongoose-encryption');
+const md5 = require('md5');
+//const encrypt=require('mongoose-encryption');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -21,8 +22,9 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-const secret=process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret,encryptedFields: ['paassword']});
+
+// use for password encrypt
+//userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ['paassword']});
 
 const User = mongoose.model("User", userSchema);
 
@@ -41,7 +43,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(err => {
         if (!err) {
@@ -54,7 +56,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const email = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({ email: email }, (err, foundUser) => {
         if (err) {
             console.log(err);
